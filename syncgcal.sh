a1fb264af057809994c6fb41244fdb1ff43237c8
piercegwang <piercegwang@users.noreply.github.com>
2020-08-26 12:11:05 -0700

    Added template syncgcal.sh file

---
+++ syncgcal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# customize these
+ACTIVATE="/ohsics_to_org/ohsical_to_org-env/bin/activate"
+ICS2ORG="/ohsics_to_org/src/main.py"
+ORGIGNOREFILE="file.org"
+ICSFILE="/ohsics_to_org/data/ohs_assignments.ics"
+ORGFILE="/gcal.org"
+URL="Link to ICS file for curling"
+
+curl $URL -o $ICSFILE
+source $ACTIVATE
+python3 $ICS2ORG $ICSFILE $ORGFILE -oi $ORGIGNOREFILE -td 70 -ig "Class to ignore"
